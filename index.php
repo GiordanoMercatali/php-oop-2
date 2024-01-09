005e39e5808ca32a0336c6f4e30991f4016945bd
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2024-01-09 18:31:10 +0100

    fixed: included toys and kennels in index

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 require_once __DIR__ . "/models/Category.php";
 require_once __DIR__ . "/models/Product.php";
 require_once __DIR__ . "/models/Food.php";
+require_once __DIR__ . "/models/Toy.php";
+require_once __DIR__ . "/models/Kennel.php";
 
 $dog = new Category ('cane', 'dog.png');
 // $dog->setName('cane');
